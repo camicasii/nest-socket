959eed935e02abac1513e83ba28d0ae9cf26e8b0
Jhonattan  Ramirez <camicasii@hotmail.com>
2024-11-14 21:44:11 -0500

    adasD

---
+++ src/event/event.gateway.ts
@@ -0,0 +1,58 @@
+import {
+  ConnectedSocket,
+  MessageBody,
+  SubscribeMessage,
+  WebSocketGateway,
+  WebSocketServer,
+  WsResponse,
+} from '@nestjs/websockets';
+import { from, Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { Server } from 'socket.io';
+import { EventService } from './event.service';
+import { CreateEventDto } from './dto/create-event.dto';
+import { UpdateEventDto } from './dto/update-event.dto';
+import { Socket } from 'dgram';
+
+@WebSocketGateway()
+export class EventGateway {
+  @WebSocketServer()
+  server: Server;
+  constructor(private readonly eventService: EventService) {}
+
+
+  @SubscribeMessage('ping')
+  ping(@MessageBody() createEventDto: any,  @ConnectedSocket() client: Socket,) {
+
+    console.log(createEventDto);
+    
+    // return this.eventService.create(createEventDto);
+  client.emit('pong', 'pong');
+    return "pong";
+  }
+
+  @SubscribeMessage('createEvent')
+  create(@MessageBody() createEventDto: CreateEventDto) {
+    return this.eventService.create(createEventDto);
+  }
+
+  @SubscribeMessage('findAllEvent')
+  findAll() {
+    return this.eventService.findAll();
+  }
+
+  @SubscribeMessage('findOneEvent')
+  findOne(@MessageBody() id: number) {
+    return this.eventService.findOne(id);
+  }
+
+  @SubscribeMessage('updateEvent')
+  update(@MessageBody() updateEventDto: UpdateEventDto) {
+    return this.eventService.update(updateEventDto.id, updateEventDto);
+  }
+
+  @SubscribeMessage('removeEvent')
+  remove(@MessageBody() id: number) {
+    return this.eventService.remove(id);
+  }
+}
